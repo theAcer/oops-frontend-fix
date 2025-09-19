f730c4bf012cfa17528c7cf634c53237a1c759b8
theAcer <samsonnjogu@gmail.com>
2025-09-19 15:03:26 +0300

    service startups script

---
+++ backend/wait-for-it.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# wait-for-it.sh
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "$host" -p "$port" -U postgres
+do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+# Wait for Redis
+until redis-cli -h redis ping;
+do
+  >&2 echo "Redis is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Redis is up - executing command"
+
+exec $cmd 

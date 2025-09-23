05c97eec129dc7327eed8a25759199fb0a5b4d5d
theAcer <samsonnjogu@gmail.com>
2025-09-23 11:46:18 +0300

    is2

--- backend/wait-for-it.sh
+++ backend/wait-for-it.sh
@@ -6,7 +6,8 @@ set -ex # Added -x for shell debugging
 host="$1"
 port="$2"
 shift 2
-cmd="$@"
+# Capture the remaining arguments as a single command string to preserve operators like '&&'
+cmd="$*"
 
 until PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "$host" -p "$port" -U postgres
 do
@@ -25,4 +26,5 @@ done
 
 >&2 echo "Redis is up - executing command"
 
-exec $cmd 
+# Execute the command string via a login shell to interpret operators properly
+exec /bin/bash -lc "$cmd" 
